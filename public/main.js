647b00dbb40907955a91f9c277bb65e78b9ea01c
Aki <aki.eronen@gmail.com>
2025-05-02 22:55:11 +0300

    modifications

--- public/main.js
+++ public/main.js
@@ -61,12 +61,21 @@ function hostRoom() {
     alert("Please enter a room name");
     return;
   }
-  socket.send(JSON.stringify({ type: 'nickname', nickname }));
+  safeSend(JSON.stringify({ type: 'nickname', nickname }));
   setTimeout(() => {
-    socket.send(JSON.stringify({ type: 'host', room: roomInput.value }));
+    safeSend(JSON.stringify({ type: 'host', room: roomInput.value }));
     showGame();
   }, 200);
 }
+
+function safeSend(data) {
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(data);
+  } else {
+    alert("Yhteys palvelimeen on katkennut. Lataa sivu uudelleen.");
+  }
+}
+
 
 // Function to handle joining a room
 function joinRoom() {
